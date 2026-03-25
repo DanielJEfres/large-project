0ff55a40cb8dfc0ca16496adb027c5b337a338ef
DanielJEfres <da947619@ucf.edu>
2026-03-24 20:34:16 -0400

    feat: added tags mongoose

--- app/backend/models/Tag.js
+++ app/backend/models/Tag.js
@@ -1,1 +1,29 @@
 import mongoose from 'mongoose'
+
+const tagSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true, 
+    trim: true,
+    unique: true,
+    lowercase: true
+  },
+  isCustom: { 
+    type: Boolean, 
+    default: false
+  },
+  isApproved: { 
+    type: Boolean, 
+    default: false
+  },
+  createdBy: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Organization',
+    default: null
+  }
+}, { timestamps: true })
+
+tagSchema.index({ name: 'text' })
+tagSchema.index({ isApproved: 1 })
+
+export default mongoose.model('Tag', tagSchema)
