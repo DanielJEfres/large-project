dddacd35ca03f852d6350f1ee1ce61b813e04dc6
Adammouedden <adam.mouedden@gmail.com>
2026-03-27 15:29:43 -0400

    Resolved conflicts

--- app/backend/routes/index.js
+++ app/backend/routes/index.js
@@ -5,13 +5,8 @@ import authRoutes from './auth.js'
 const router = express.Router()
 const app = express()
 
-<<<<<<< HEAD
 // router.use('/', testRoutes)
 app.use('/', testRoutes)
 app.use('/api/auth', router)
-=======
-router.use('/test', testRoutes)
-router.use('/', authRoutes)
->>>>>>> f0799c986d00a21f740cbf523447daf53d0d47c4
 
 export default router
