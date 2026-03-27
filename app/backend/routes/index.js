368f799ab9b55b3a5a17eba1f7d7378f3cf4201a
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2026-03-26 21:20:02 -0400

    feat: connect signup frontend to backend and add auth route to index.js

--- app/backend/routes/index.js
+++ app/backend/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import testRoutes from './test.js'
+import authRoutes from './auth.js'
 
 const router = express.Router()
 
 router.use('/', testRoutes)
+router.use('/', authRoutes)
 
 export default router
