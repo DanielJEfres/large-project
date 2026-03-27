7b8facdacbe936f3ffdb5e841f0b493adab7a5ff
Amy621 <amyhakim5253@gmail.com>
2026-03-27 00:25:56 -0400

    feat(web): initialize vite project with react and tailwind v4
    Created project structure in app/web, set up vite build system and typescript config, config tailwind css with custom @theme breakpoints and fonts, added routing for main.tsx and login page, created login page with custom stylings and responsive breakpoints

---
+++ app/web/src/main.tsx
@@ -0,0 +1,16 @@
+import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+
+
+createRoot(document.getElementById('root')!).render( 
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+) 
