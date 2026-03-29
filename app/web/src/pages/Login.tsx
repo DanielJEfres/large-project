5d2b18863c58e74d8afae2fd9500cfd7f83a3ac0
Amy621 <amyhakim5253@gmail.com>
2026-03-28 21:55:33 -0400

    feat: add EventKnight logo to login page and responsiveness

--- app/web/src/pages/Login.tsx
+++ app/web/src/pages/Login.tsx
@@ -1,14 +1,13 @@
+import styles from './Login.module.css'
+import Logo from '../components/Logo'
 import React, { useState, type ChangeEvent } from "react";
 import { Link } from "react-router";
 import { X, Eye, EyeOff, Info } from "lucide-react";
-import styles from "./Login.module.css";
-import Logo from "../components/Logo.tsx";
 
 interface LoginFormData {
   email: string;
   password: string;
 }
-
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState<string>("");
@@ -76,35 +75,18 @@ export default function Login() {
     }
   };
 
-  const isFieldError = (fieldKeyword: string) =>
-    error.toLowerCase().includes(fieldKeyword.toLowerCase());
+  /*const isFieldError = (fieldKeyword: string) =>
+    error.toLowerCase().includes(fieldKeyword.toLowerCase()); */
 
   return (
     <>
       <div className="[&_button]:cursor-pointer fixed inset-0 bg-white flex flex-col justify-center items-center z-50">
+        <Link
+          to="/"
+          className="absolute top-6 left-6 w-10 h-10 rounded-full bg-black flex items-center justify-center text-white text-xl"
+        ></Link>
         <div className="w-95">
-          <div className={styles.logo}>
-            <Logo />
-          </div>
-
-          {/* welcome title */}
-          <div className={styles.titleWrapper}>
-            <h1 className="font-bebas text-5xl text-center">Welcome back</h1>
-            <svg
-              width="34"
-              height="55"
-              viewBox="0 0 39 61"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.5613 4.75128C11.5613 9.08789 10.571 20.3496 8.87909 24.5587C7.23105 28.6587 3.44331 31.7538 2.67801 33.0706C1.30983 35.4246 8.30137 33.7571 9.98575 34.8169C13.9526 37.3127 16.6782 44.4786 17.8112 51.7394C18.14 53.8467 17.9387 56.4681 17.9387 57.0645C17.9387 60.4768 19.0566 49.663 23.7909 43.7283C30.2068 35.6856 35.8141 35.8916 36.3806 35.267C36.9719 34.6151 32.2203 33.0293 27.5385 29.4749C23.971 25.7966 20.5835 18.8078 17.7699 10.127C16.8133 6.52194 16.8133 4.5412 16.4381 2.50044"
-                stroke="black"
-                strokeWidth="5"
-                strokeLinecap="round"
-              />
-            </svg>
-          </div>
+          <h2 className="font-bebas text-5xl text-center">Welcome back</h2>
 
           <form
             onSubmit={handleSubmit}
