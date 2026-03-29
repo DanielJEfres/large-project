edb3ec80d4555c6e38952ec0b2dce53863e0b5bd
sofiaflores18 <hellofloressofia@gmail.com>
2026-03-28 21:43:00 -0400

    get Events by date (upcoming) and by tag

--- app/backend/routes/index.js
+++ app/backend/routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import testRoutes from './test.js'
 import authRoutes from './auth.js'
+import getEventsRoutes from './getEvents.js'
 
 const router = express.Router()
 
 // router.use('/', testRoutes)
 router.use('/', testRoutes)
 router.use('/api/auth', authRoutes)
-
+router.use('/api/getEvents', getEventsRoutes)
 export default router
